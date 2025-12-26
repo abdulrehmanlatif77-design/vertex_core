6315aed7c62a1a69ca1bbf832f35571f4c209781
rajeshkumar1221 <rajeshkumar1221@gmail.com>
2025-12-26 11:57:51 +0530

    created components and service for each entity

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
+import { Login } from '../pages/login/login';
+import { Student } from '../pages/student/student';
+import { Course } from '../pages/course/course';
+import { Department } from '../pages/department/department';
+import { Instructor } from '../pages/instructor/instructor';
+import { Section } from '../pages/section/section';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch:'full'
+    },
+    {
+        path:'login',
+        component:Login
+    },
+    {
+        path:'',
+        component:Login,
+        children:[
+            {
+                path:'student',
+                component:Student
+            },
+            {
+                path:'course',
+                component:Course
+            },
+            {
+                path:'department',
+                component:Department
+            },
+            {
+                path:'insturctor',
+                component:Instructor
+            },
+            {
+                path:'section',
+                component:Section
+            }
+        ]
+    }
+];
