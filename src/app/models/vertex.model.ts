76ccc29ce7b1b00b2e9293fe863464555fa24858
rajeshkumar1221 <rajeshkumar1221@gmail.com>
2025-12-26 19:27:07 +0530

    created the model

--- src/app/models/vertex.model.ts
+++ src/app/models/vertex.model.ts
@@ -9,3 +9,35 @@ export class LoginModel
         this.password = '';
     }
 }
+
+export class StudentModel
+{
+    id: string;
+    emailAddress: string;
+    firstName: string;
+    lastName: string;
+    middleInitial: string;
+    dateOfBirth: Date;
+    gender: string;
+    address: string;
+    phoneNumber: string;
+    enrollmentDate: Date;
+    degree:number;
+    gpa: number;
+
+    constructor(id: string, emailAddress: string, firstName: string, lastName: string, middleInitial: string, dateOfBirth: Date, gender: string, address: string, phoneNumber: string, enrollmentDate: Date, degree: number, gpa: number)
+    {
+        this.id = id;
+        this.emailAddress = emailAddress;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleInitial = middleInitial;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.enrollmentDate = enrollmentDate;
+        this.degree = degree;
+        this.gpa = gpa;
+    }
+}
