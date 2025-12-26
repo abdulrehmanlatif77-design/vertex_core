7d980d857ae143144aaec9c72dd21607a637d5d8
rajeshkumar1221 <rajeshkumar1221@gmail.com>
2025-12-26 19:39:59 +0530

    Able to fetch the students data

--- src/app/pages/student/student.ts
+++ src/app/pages/student/student.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Student as StudentService } from '../../services/student/student';
+import { StudentModel } from '../../models/vertex.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-student',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './student.html',
   styleUrl: './student.css',
 })
 export class Student {
+  constructor(private studentService: StudentService) {}
 
+  students: StudentModel[] = [];  
+
+  ngOnInit() {
+    this.loadStudents();
+  } 
+  loadStudents() {
+    this.studentService.getStudents().subscribe({
+      next: (res: any) => {
+        this.students = res.data;
+      },
+      error: (err:any) => {
+        console.log(err);
+      },
+    });
+  }
 }
